03a6f4415491af38d460cc57ffd5d0517f0322c1
cong <q1875486458@gmail.com>
2023-06-05 14:40:03 +0800

    add mini config

--- actions/actions.sh
+++ actions/actions.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 function br-note () {
+    # 1. generate config
+    #    use x86_64
+    #    use ext4 fs
+    #    install package like openssh tcpdump iptables etc..
+    #    you could use preconfiged config file like ./.config.mini
+    # 2. unset LD_LIBRARY_PATH
+    # 3. make -j10
     return
 }
